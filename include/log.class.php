6d342a78e4a8219354ee21919c1adea29bb27da2
huashen <huashen361@gmail.com>
2014-06-15 21:26:47 +0800

    添加日志类

---
+++ include/log.class.php
@@ -0,0 +1,55 @@
+<?php
+/*
+*日志类
+*/
+class Log
+{
+	const LOG_FILE = 'curr.log';
+
+	/*
+	*写日志
+	*/
+	public static function write($content) 
+	{
+		$content .= "\r\n";
+		$log = self :: isBak();
+
+		$fh = fopen($log, 'ab');
+		fwrite($fh, $content);
+		fclose($fh);
+	}
+
+	/*
+	*备份日志
+	*/
+	private static function bak()
+	{
+		$log = ROOT . 'data/log/' . self :: LOG_FILE;
+		$bak = ROOT . 'data/log/' . date('ymd') . mt_rand(10000,9999) . '.bak';
+		return rename($log, $bak);
+	}
+
+	/*
+	*判断并读取日志大小
+	*/
+	public static function isBak()
+	{
+		$log = ROOT . 'data/log/' . self :: LOG_FILE;
+		if(!file_exists($log)) {
+			touch($log);
+			return $log;
+		}
+
+		$size = filesize($log);
+		if($size <= 1024 * 1024) {
+			return $log;
+		}
+
+		if(!self :: bak()) {//备份失败
+			return $log;
+		}else {//备份成功
+			touch($log);
+			return $log;
+		}
+	}
+}
